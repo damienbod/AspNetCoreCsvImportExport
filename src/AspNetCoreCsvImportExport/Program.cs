f6e910404ec5827db559d3d48c78f3b2f44dcc26
damienbod <damien_bod@hotmail.com>
2017-08-23 07:43:30 +0200

    Updating program

--- src/AspNetCoreCsvImportExport/Program.cs
+++ src/AspNetCoreCsvImportExport/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
 
 namespace AspNetCoreCsvImportExport
 {
@@ -12,14 +7,12 @@ namespace AspNetCoreCsvImportExport
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
